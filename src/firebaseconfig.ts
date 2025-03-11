b64f93a66634020452083428483c003633240106
SamosTheWiseSage <GE.kelemwork@gmail.com>
2025-03-11 12:13:31 +0100

    firebase secret fix by env

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -1,22 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, get } from 'firebase/database';
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyC5CZw5jbWHfK9kcb7L0ExQUvdoHuCpBos",
-    authDomain: "gachaslotstorage.firebaseapp.com",
-    databaseURL: "https://gachaslotstorage-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "gachaslotstorage",
-    storageBucket: "gachaslotstorage.firebasestorage.app",
-    messagingSenderId: "575628586857",
-    appId: "1:575628586857:web:0ba9bb7ea01e6b4a02d7f8"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-
 const database = getDatabase(app);
-
 
 export { database, ref, get };
